7c20fe5ce741cb1fcbfeb8233b015a8d4848af61
dkn1fe <dknifeb@mail.ru>
2024-09-05 21:00:58 +0300

    feat: add glavs page

--- src/features/choose-mode/ui/Choose-Mode.tsx
+++ src/features/choose-mode/ui/Choose-Mode.tsx
@@ -1,11 +1,17 @@
+import { FC } from "react"
 import { Modes } from "./Modes"
 
-export const ChooseMode = () => {
+interface ChooseModeProps{
+     modes:any[],
+     title:string
+}
+
+export const ChooseMode:FC<ChooseModeProps> = ({modes,title}) => {
     return (
         <div className="max-w-[600px] m-auto pt-24">
             <div className="w-full h-[483px] bg-white p-2 shadow-2xl">
-                <h3 className="pl-2">Выберите режим</h3>
-                <Modes />
+                <h3 className="pl-2">{title}</h3>
+                <Modes modes={modes} title = {title}/>
             </div>
         </div>
     )
